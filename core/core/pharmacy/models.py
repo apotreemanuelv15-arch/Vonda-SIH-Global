85f6a49c68f2615abd340a446a3a8dcef65b9217
apotreemanuelv15-arch <apotreemanuelv15@gmail.com>
2026-04-30 14:56:17 +0200

    Update models.py

--- core/core/pharmacy/models.py
+++ core/core/pharmacy/models.py
@@ -10,3 +10,13 @@ class Medicament(models.Model):
 
     def __str__(self):
         return self.nom
+
+class Vente(models.Model):
+    # On lie la vente à un médicament précis
+    medicament = models.ForeignKey(Medicament, on_delete=models.CASCADE)
+    quantite_vendue = models.IntegerField()
+    prix_total = models.DecimalField(max_digits=10, decimal_places=2)
+    date_vente = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Vente de {self.quantite_vendue} {self.medicament.nom}"
