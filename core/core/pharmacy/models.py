d8983aa5d8a48324706309965df6a8fc6abfd639
apotreemanuelv15-arch <apotreemanuelv15@gmail.com>
2026-04-30 15:25:30 +0200

    Update models.py

--- core/core/pharmacy/models.py
+++ core/core/pharmacy/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+# --- MODULE PHARMACIE ---
 class Medicament(models.Model):
     nom = models.CharField(max_length=200)
     description = models.TextField(blank=True)
@@ -12,11 +13,21 @@ class Medicament(models.Model):
         return self.nom
 
 class Vente(models.Model):
-    # On lie la vente à un médicament précis
     medicament = models.ForeignKey(Medicament, on_delete=models.CASCADE)
     quantite_vendue = models.IntegerField()
     prix_total = models.DecimalField(max_digits=10, decimal_places=2)
     date_vente = models.DateTimeField(auto_now_add=True)
 
+# --- MODULE PATIENTS ---
+class Patient(models.Model):
+    SEXE_CHOICES = [('M', 'Masculin'), ('F', 'Féminin')]
+    
+    nom_complet = models.CharField(max_length=255)
+    date_naissance = models.DateField()
+    sexe = models.CharField(max_length=1, choices=SEXE_CHOICES)
+    adresse = models.TextField(blank=True)
+    telephone = models.CharField(max_length=20, blank=True)
+    date_enregistrement = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
-        return f"Vente de {self.quantite_vendue} {self.medicament.nom}"
+        return self.nom_complet
