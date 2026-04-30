2cb6b973dfee744e247aea003547fb47a624cbbe
apotreemanuelv15-arch <apotreemanuelv15@gmail.com>
2026-04-30 13:53:59 +0200

    Update urls.py

--- core/core/pharmacy/pharmacy/pharmacy/pharmacy/urls.py
+++ core/core/pharmacy/pharmacy/pharmacy/pharmacy/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Nous ajouterons les liens vers vos pages ici bientôt
+    # Cette ligne lie l'adresse 'inventaire/' à votre fonction dans views.py
+    path('inventaire/', views.liste_medicaments, name='liste_medicaments'),
 ]
