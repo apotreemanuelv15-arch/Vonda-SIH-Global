8ca72d9b4da035214ae663cf672cd63ad1a76a18
apotreemanuelv15-arch <apotreemanuelv15@gmail.com>
2026-04-30 15:26:59 +0200

    Update forms.py

--- pharmacy/forms.py
+++ pharmacy/forms.py
@@ -1,10 +1,17 @@
 from django import forms
-from .models import Medicament
+from .models import Medicament, Patient
 
 class MedicamentForm(forms.ModelForm):
     class Meta:
         model = Medicament
         fields = ['nom', 'description', 'prix', 'quantite_stock', 'date_expiration']
+        widgets = {'date_expiration': forms.DateInput(attrs={'type': 'date'})}
+
+class PatientForm(forms.ModelForm):
+    class Meta:
+        model = Patient
+        fields = ['nom_complet', 'date_naissance', 'sexe', 'adresse', 'telephone']
         widgets = {
-            'date_expiration': forms.DateInput(attrs={'type': 'date'}),
+            'date_naissance': forms.DateInput(attrs={'type': 'date'}),
+            'adresse': forms.Textarea(attrs={'rows': 2}),
         }
