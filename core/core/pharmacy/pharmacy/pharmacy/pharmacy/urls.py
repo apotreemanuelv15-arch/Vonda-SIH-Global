9571c39349cc2705001aa760fcd6f447d902ef8c
apotreemanuelv15-arch <apotreemanuelv15@gmail.com>
2026-04-30 14:59:48 +0200

    Update urls.py

--- core/core/pharmacy/pharmacy/pharmacy/pharmacy/urls.py
+++ core/core/pharmacy/pharmacy/pharmacy/pharmacy/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Chemin pour la liste : http://.../pharmacy/inventaire/
     path('inventaire/', views.liste_medicaments, name='liste_medicaments'),
-    
-    # Chemin pour l'ajout : http://.../pharmacy/ajouter/
     path('ajouter/', views.ajouter_medicament, name='ajouter_medicament'),
+    # Route pour vendre un médicament spécifique via son ID
+    path('vendre/<int:pk>/', views.effectuer_vente, name='effectuer_vente'),
 ]
