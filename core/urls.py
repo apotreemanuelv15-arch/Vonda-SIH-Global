3979703f9a09c5245be9f9c52e110072990d9663
apotreemanuelv15-arch <apotreemanuelv15@gmail.com>
2026-05-24 21:25:26 +0200

    Correction de l'aiguillage de la page d'accueil vers inventaire

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView  # Pour rediriger automatiquement
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pharmacy.urls')),  # Oriente directement vers votre pharmacie en page d'accueil
+    path('', RedirectView.as_view(url='inventaire/', permanent=True)), # Si on tape l'adresse brute, hop ! Direction l'inventaire
+    path('', include('pharmacy.urls')), 
 ]
