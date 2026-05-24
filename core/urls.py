8e8acf05feb7919cacb87f4a9199b1e77d76729e
apotreemanuelv15-arch <apotreemanuelv15@gmail.com>
2026-05-24 21:35:10 +0200

    Correction de la route de connexion accounts/login

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView  # Pour rediriger automatiquement
+from django.views.generic import RedirectView
+from django.contrib.auth import views as auth_views  # Moteur de connexion Django
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', RedirectView.as_view(url='inventaire/', permanent=True)), # Si on tape l'adresse brute, hop ! Direction l'inventaire
+    
+    # 1. Redirection automatique de la racine vers l'inventaire
+    path('', RedirectView.as_view(url='inventaire/', permanent=True)), 
+    
+    # 2. Route officielle que Django réclame pour la connexion (Correction de l'erreur 404)
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='admin/login.html'), name='login'),
+    
+    # 3. Les routes de vos applications métiers
     path('', include('pharmacy.urls')), 
 ]
